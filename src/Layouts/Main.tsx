a0543a7951e09ff0c69e4c04c0ae167f7be5ac16
Adjanour <adjanour@icloud.com>
2024-01-06 08:16:02 +0000

    Add @vercel/speed-insights package and component

--- src/Layouts/Main.tsx
+++ src/Layouts/Main.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Navbar } from "../Components/Navbar";
 import { navItems } from "../Components/Navbar";
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 type MainLayoutProps = {
   children: React.ReactNode;
@@ -10,11 +11,14 @@ export const MainLayout: React.FC<MainLayoutProps> = ({
   children,
 }: MainLayoutProps) => {
   return (
+    <>
+    <SpeedInsights/>
     <div className="bg-slate-100 dark:bg-black dark:text-white">
       <Navbar items={navItems} />
       <main>
         {children}
       </main>
     </div>
+    </>
   );
 };
