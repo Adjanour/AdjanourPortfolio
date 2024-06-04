3aaabc90dbfbd97bb7e40e6576aeb75546082a7b
katamansokirk <katamansokirk@gmail.com>
2024-06-04 13:39:41 +0000

    chore: fix navbar links

--- src/Components/Navbar/Navbar.tsx
+++ src/Components/Navbar/Navbar.tsx
@@ -30,7 +30,7 @@ export const Navbar: React.FC<NavbarProps> = ({ items, className }: NavbarProps)
              key={index}
            >
             <a href={item.to}>
-              <Button text={item.text} className='px-2 text-black py-1' />
+              <Button text={item.text} className='px-2 bg-white text-black py-1' />
             </a>
             </motion.button>
           ))}
